f7d9e194d1c235a7f5596d8913eef0d0d37c858c
bison-- <mike.bison42@googlemail.com>
2013-04-29 23:03:27 +0200

    splitting main into main/test

--- main.py
+++ main.py
@@ -5,30 +5,6 @@ import sys
 import datetime
 import math
 
-class test(object): # a class to test the main functions
-	def __init__(self):
-		self.mainC = main()
-		self.world = self.mainC.newWorld()
-		self.player = self.mainC.newPlayer('test',0,1,1000000,1,0,0,0)
-		self.figures = {}
-		self.figures = self.mainC.defaultFigures(self.figures)
-	def testTrade(self,player,figDemand):
-		money = player['money']
-		inv = player['inv']
-		tradingProfit = self.mainC.trade(self.figures,money,figDemand,inv)
-		player['money'] = tradingProfit['money']
-		player['inv'] = tradingProfit['inv']
-		return player
-	def autoRandomFillInventory(self,length,player):
-		for i in range(1,length+1):
-			player = self.testTrade(player,random.choice(['w','s','r']))
-		return player
-	def testLevelTest(self,player):
-		player['level'] = self.mainC.levelTest(self.mainC.experienceGen(),player['ep'],player['level'])
-		return player
-	def testGiveEP(self,player,epRangeMin,epRangeMax):
-		player = self.mainC.giveEP(player,random.randint(epRangeMin,epRangeMax))
-		return player
 		
 class main(object):
 	def giveEP(self,player,ep):
@@ -185,10 +161,6 @@ class main(object):
 			inv['r'] += 1
 		return {'money':gold,'inv':inv}
 
-testC = test()
-player = testC.player
-enemy = testC.mainC.newPlayer('enemy',20134,1,1000000,1,0,0,0)
-# print testC.mainC.printDict(testC.autoRandomFillInventory(50,player))
-enemy = testC.autoRandomFillInventory(50,enemy)
-enemy = testC.testGiveEP(enemy,100,10000)
-print testC.mainC.printDict(testC.testLevelTest(enemy))
+if __name__ == '__main__':
+	print 'nothing at all, yet'
+
